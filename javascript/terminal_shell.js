37782509487ef5d7cf694f386ef4abbeb8378b07
Eric Gonzalez <ericgonzadev@gmail.com>
2023-06-30 18:56:19 -0700

    Text changes; Minor styling changes

--- javascript/terminal_shell.js
+++ javascript/terminal_shell.js
@@ -1,8 +1,8 @@
 // setup typewriter effect in the terminal demo
 if (document.getElementsByClassName('demo').length > 0) {
   var i = 0;
-  var txt = "I'm adept with a majority of web development frameworks such as Django and Ruby on Rails. I usually work on both fronts of a web application and have no issues jumping back the front-end and back-end. I have experience setting up and maintaining web apps to industry standards with tools such as Git, AWS, Testing Services (Sentry, Papertrail), Heroku, Google Services, etc. All my work experience can be found below.";
-  var speed = 20;
+  var txt = "I possess expertise in a wide range of web development frameworks such as Django and Ruby on Rails. With a comprehensive understanding of both front-end and back-end development, I can seamlessly navigate between the two with ease. Throughout my experience, I have successfully established and maintained web applications that adhere to industry standards with tools such as Git, AWS, Testing Services (Sentry, Papertrail), Heroku, Google Services, and more. For a detailed overview of my work experience, please refer to the section below.";
+  var speed = 5;
 
   function typeItOut () {
     if (i < txt.length) {
